8e56ce3d506f5a8e38ed429758f90707a66b9792
PIERRE_WIART <PWIART89@gmail.com>
2025-03-31 23:33:59 +0200

    exercice-4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,20 +1,10 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
 
 function ImgPokemon({ imgSrc }: { imgSrc?: string }) {
   return imgSrc ? <img src={imgSrc} alt="illu du pokemon" /> : <p>???</p>;
 }
 
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       <ImgPokemon imgSrc={pokemon.imgSrc} />
