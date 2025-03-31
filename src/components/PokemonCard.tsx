16a87b27e84c47f8e7c2c7897258b53f23584511
PIERRE_WIART <PWIART89@gmail.com>
2025-04-01 00:52:55 +0200

    exercices 5

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -4,7 +4,13 @@ function ImgPokemon({ imgSrc }: { imgSrc?: string }) {
   return imgSrc ? <img src={imgSrc} alt="illu du pokemon" /> : <p>???</p>;
 }
 
-function PokemonCard({ pokemon }) {
+interface Pokemon {
+  name : string;
+  imgSrc?: string;
+}
+
+function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+
   return (
     <figure>
       <ImgPokemon imgSrc={pokemon.imgSrc} />
